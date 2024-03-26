eae297fd7e62429d92d3d54d76a86f29a93a4297
David R Anderson <dranderson@google.com>
2024-03-26 15:33:48 +0000

    Corrected substitution of Bucket name in command.

--- scripts/simple_setup.sh
+++ scripts/simple_setup.sh
@@ -19,7 +19,8 @@ pip install pydicom scikit-learn "tf-models-official==2.14.0" > /dev/null # only
 
 # Create Model Bucket
 printf "***\n* Creating and GCS Bucket to store trained model artifacts\n***\n"
-gcloud storage buckets create gs://{$MODEL_BUCKET_NAME} --project=$PROJECT_ID
+bucketName = "gs://" + $MODEL_BUCKET_NAME
+gcloud storage buckets create $bucketName --project=$PROJECT_ID
 
 # Create and populate DICOM store
 printf "***\n* Creating and populating Test DICOM Store\n***\n"
